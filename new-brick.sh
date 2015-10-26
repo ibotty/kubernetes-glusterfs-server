892dedebd1168c8739bbfe972ec68e25dc08a576
Tobias Florek <tob@butter.sh>
2015-10-26 18:27:59 +0100

    fix names and missing options in new-brick.sh

--- new-brick.sh
+++ new-brick.sh
@@ -3,10 +3,11 @@
 set -e
 
 GLUSTER_VG=${GLUSTER_VG-fedora}
+GLUSTER_POOL=${BRICK_NAME-'gluster-pool'}
 BRICK_NAME=${BRICK_NAME-''}
 
 mk_brick() {
-    lvcreate -TV 1G -n $BRICK_NAME $GLUSTER_VG/$THIN_POOL
+    lvcreate -TV 1G -n $BRICK_NAME $GLUSTER_VG/$GLUSTER_POOL
     mkfs.xfs /dev/$GLUSTER_VG/$BRICK_NAME -L $BRICK_NAME
     mount -t xfs /dev/$GLUSTER_VG/$BRICK_NAME $BRICK_NAME
 }
@@ -18,15 +19,18 @@ mount_brick() {
 
 usage() {
     cat <<EOF 
-$0 [-v GLUSTER_VG] [BRICK_NAME]
-where GLUSTER_VG is the volume group to use
-and BRICK_NAME is the brick name to use.
-Both can be specified by environment variables.
+$0 [-v GLUSTER_VG] [-p GLUSTER_POOL] [BRICK_NAME]
+where 
+ * GLUSTER_VG is the volume group to use,
+ * GLUSTER_POOL is the thin pool to use, and
+ * BRICK_NAME is the brick name to use.
+All options can be specified by environment variables.
+GLUSTER_VG defaults to fedora, GLUSTER_POOL to gluster-pool.
 EOF
 }
 
 main() {
-    while getopts "v:" flag
+    while getopts "v:p:" flag
     do
         case "$flag" in
         v)
@@ -35,6 +39,13 @@ main() {
                 exit 1
             fi
             GLUSTER_VG=$OPTARG
+            ;;
+        p)
+            if [ -z ${OPTARG} ]; then
+                err "Missing argument GLUSTER_POOL for -p"
+                exit 1
+            fi
+            GLUSTER_POOL=$OPTARG
             ;;
         *)
             err "invalid option: $flag"
