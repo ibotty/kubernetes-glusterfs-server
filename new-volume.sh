0e04f99d55c61060cf1b0e6266823eb1420ceb3f
Tobias Florek <tob@butter.sh>
2015-10-27 11:50:50 +0100

    wip: new-volume.sh

---
+++ new-volume.sh
@@ -0,0 +1,91 @@
+#!/bin/sh
+set -e
+
+BRICK_NAME=${BRICK_NAME-''}
+VOL_NAME=${VOL_NAME-${BRICK_NAME}}
+HOSTS=""
+CREATE_OPTIONS="redundancy=1"
+VOL_OPTIONS="nfs.disable=True"
+
+start_volume=0
+
+create_vol() {
+    bricks=""
+    create_opts=${${CREATE_OPTIONS//,/ }//=/ }
+    for host in $HOSTS; do
+        bricks="$bricks $host:/bricks/$BRICK_NAME/b"
+    done
+    eval gluster volume create $VOL_NAME $opts $bricks
+
+    for opt in $VOL_OPTIONS; do
+        eval gluster volume set $VOL_NAME ${opt/=/ }
+    done
+}
+
+usage() {
+    cat <<EOF 
+$0 [-b BRICK_NAME] [-s] [-c key=value,key=value] [-o key=value,key=value] \
+   [-n VOL_NAME] host1 [host2 ...]
+where 
+ * BRICK_NAME is the name of the brick to add to the volume on each host,
+ * VOL_NAME is the name of the volume to create, defaults to BRICK_NAME,
+ * -s specifies to start the volume, defaults, not to start,
+ * -c options to set when creating volume, defaults to "redundancy=1",
+ * -o options to set with 'gluster volume set', defaults to "nfs.disable=True".
+BRICK_NAME and VOL_NAME can be specified by environment variables.
+EOF
+}
+
+main() {
+    while getopts "n:b:c:s" flag
+    do
+        case "$flag" in
+        n)
+            if [ -z ${OPTARG} ]; then
+                err "Missing argument VOL_NAME for -v"
+                exit 1
+            fi
+            VOL_NAME=$OPTARG
+            ;;
+        c)
+            if [ -z ${OPTARG} ]; then
+                err "Missing argument CREATE_OPTIONS for -c"
+                exit 1
+            fi
+            CREATE_OPTIONS=$OPTARG
+            ;;
+        o)
+            if [ -z ${OPTARG} ]; then
+                err "Missing argument VOL_OPTIONS for -o"
+                exit 1
+            fi
+            VOL_OPTIONS=$OPTARG
+            ;;
+        b)
+            if [ -z ${OPTARG} ]; then
+                err "Missing argument BRICK_NAME for -s"
+                exit 1
+            fi
+            BRICK_NAME=$OPTARG
+            ;;
+        s)
+            start_volume=1
+            ;;
+        *)
+            err "invalid option: $flag"
+            usage
+            exit 1
+        esac
+    done
+    shift $(( OPTIND - 1 ));
+
+    if [ $# -eq 0 ] ; then
+        err "Missing arguments: host"
+        exit 1
+    fi
+    HOSTS=$*
+
+    create_vol
+}
+
+main
